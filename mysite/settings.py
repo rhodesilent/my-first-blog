c6be0c83fb9d4447d6432b324de4a250de724cb5
Kimal <kim.shekhar@gmail.com>
2020-02-07 14:01:16 +0530

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/home/sachin/django-apps/blog/templates/blog'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
